b41fa4bcfaeb8b56c968b497082a3d749bcc6645
Smit T <smittrambadia@csu.fullerton.edu>
2022-06-20 12:30:43 -0700

    Initial Commit

---
+++ Merge/Merge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Merge
+//
+//  Created by csuftitan on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Merge/Merge/MergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergeApp.swift
+//  Merge
+//
+//  Created by csuftitan on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
